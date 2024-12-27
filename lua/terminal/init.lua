83f4039d1c845f074a655744bebedd2291989a34
Ojukwu Ezechukwu <ezechukwu69@gmail.com>
2024-12-27 15:16:05 +0100

    Update to close terminal once done

--- lua/terminal/init.lua
+++ lua/terminal/init.lua
@@ -61,11 +61,36 @@ M.create_terminal = function()
 		vim.bo[buf].buflisted = false
 	end
 
+	-- detect when program running in terminal ends so we can close the terminal
+	vim.api.nvim_create_autocmd("TermClose", {
+		once = true,
+		buffer = buf,
+		callback = function()
+			M.close_terminal()
+		end,
+	})
+
 	state.terminal_id = vim.bo.channel
 	vim.cmd.startinsert()
 end
 
+--- call create terminal if the terminal_id is nil or window is not valid or buffer is not valid
+--- @return boolean
+local function create_terminal()
+	if
+		state.terminal_id == nil
+		or not vim.api.nvim_win_is_valid(state.terminal_winid)
+		or not vim.api.nvim_buf_is_valid(state.terminal_bufnr)
+	then
+		M.create_terminal()
+		return true
+	else
+		return false
+	end
+end
+
 M.close_terminal = function()
+	state.terminal_id = nil
 	vim.api.nvim_buf_delete(state.terminal_bufnr, { force = true })
 	vim.api.nvim_win_close(state.terminal_winid, true)
 end
@@ -82,10 +107,24 @@ end
 
 ---@param cmd string
 M.send_cmd = function(cmd)
-	if cmd == "" or cmd == nil then
-		return
+	local created_newly = create_terminal()
+
+	local send = function()
+		if cmd == "" or cmd == nil then
+			return
+		end
+		vim.fn.chansend(state.terminal_id, { cmd .. "\r\n" })
 	end
-	vim.fn.chansend(state.terminal_id, { cmd .. "\r\n" })
+
+	---wait for 3 second before sending the command if created_newly
+	if created_newly then
+		vim.defer_fn(function()
+			send()
+		end, 3000)
+		return
+	else
+		send()
+	end
 end
 
 M.create_command = require("terminal.command").create_command
