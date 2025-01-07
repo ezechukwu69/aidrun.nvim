d041e3778eed343fe0223fd77450e84ad310b0c2
Ojukwu Ezechukwu <ezechukwu69@gmail.com>
2025-01-07 21:27:46 +0100

    Fix for comments

--- lua/terminal/utils/comment.lua
+++ lua/terminal/utils/comment.lua
@@ -7,12 +7,6 @@ local get_commentstring = function()
 	commentstring = commentstring:gsub("%%s", "")
 
 	-- Ensure the comment string does not close prematurely
-	local first_char = commentstring:sub(1, 1)
-	local last_char = commentstring:sub(-1)
-	if first_char == last_char then
-		commentstring = first_char
-	end
-
 	return commentstring
 end
 
